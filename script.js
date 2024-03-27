eaa04db98917c873fcd0a6bc4009e54836e0563b
myuma24 <147002196+myuma24@users.noreply.github.com>
2024-03-27 02:44:19 -0700

    casestudy

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 (function(){
     'use strict'
     console.log('running js');
-});
+
+    // Select the div by its ID
+    var foodixDiv = document.getElementById('foodixID');
+    var aggieDiv = document.getElementById('AggieRewardsID');
+    var crouketDiv = document.getElementById('crouketID');
+
+    // Add a click event listener to the div
+    foodixDiv.addEventListener('click', function(event) {
+         // Redirect the user to the desired link
+        window.location.href = 'foodix.html';
+    });
+    aggieDiv.addEventListener('click', function(event) {
+        // Redirect the user to the desired link
+       window.location.href = 'https://www.example.com';
+    });
+    crouketDiv.addEventListener('click', function(event) {
+    // Redirect the user to the desired link
+        window.location.href = 'https://www.example.com';
+    });
+
+})();
