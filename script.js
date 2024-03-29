c00256233b9c8bc1c2fef22bcdb9a7e46c954ab4
Maggie <myuma@ucdavis.edu>
2024-03-28 22:58:08 -0700

    portfolio

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
     var foodixDiv = document.getElementById('foodixID');
     var aggieDiv = document.getElementById('AggieRewardsID');
     var crouketDiv = document.getElementById('crouketID');
+    var hubbleDiv = document.getElementById('hubbleleID');
 
     // Add a click event listener to the div
     foodixDiv.addEventListener('click', function(event) {
@@ -14,11 +15,15 @@
     });
     aggieDiv.addEventListener('click', function(event) {
         // Redirect the user to the desired link
-       window.location.href = 'https://www.example.com';
+       window.location.href = 'aggierewards.html';
     });
     crouketDiv.addEventListener('click', function(event) {
     // Redirect the user to the desired link
-        window.location.href = 'https://www.example.com';
+        window.location.href = 'croukets.html';
     });
-
+    hubbleDiv.addEventListener('click', function(event) {
+        // Redirect the user to the desired link
+            window.location.href = 'hubble.html';
+    });
+    
 })();
