ddd75422ba7a7d675cdf80782deb058c39d5c447
Maggie <myuma@ucdavis.edu>
2024-04-17 16:09:35 -0700

    update

--- script.js
+++ script.js
@@ -5,25 +5,21 @@
     // Select the div by its ID
     var foodixDiv = document.getElementById('foodixID');
     var aggieDiv = document.getElementById('AggieRewardsID');
-    var crouketDiv = document.getElementById('crouketID');
-    var hubbleDiv = document.getElementById('hubbleleID');
+    var hubbleDiv = document.getElementById('hubbleID');
 
     // Add a click event listener to the div
     foodixDiv.addEventListener('click', function(event) {
-         // Redirect the user to the desired link
+        // Redirect the user to the desired link
         window.location.href = 'foodix.html';
     });
     aggieDiv.addEventListener('click', function(event) {
         // Redirect the user to the desired link
        window.location.href = 'aggierewards.html';
     });
-    crouketDiv.addEventListener('click', function(event) {
-    // Redirect the user to the desired link
-        window.location.href = 'croukets.html';
-    });
+
     hubbleDiv.addEventListener('click', function(event) {
         // Redirect the user to the desired link
-            window.location.href = 'hubble.html';
+        window.location.href = 'hubbble.html';
     });
     
 })();
